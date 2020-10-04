0dde9e02a4193cb9cb1ebf21fd991fa2ca638f66
Stefan Eng <eng_steff@outlook.com>
2020-10-04 15:31:48 +0200

    Add ex1.7.scm.

---
+++ src/ex1.7.scm
@@ -0,0 +1,12 @@
+; Exercise 1.7
+; ------------
+
+; 1.) Very small numbers:
+;   An absolute cut-off of 0.001 will fail if both values are smaller than the
+;   cut-off, which always produces a value lower than the absolute cut-off.
+
+; 2.) Very large numbers:
+;   When working with large numbers the loss off precision due to limited bits
+;   can affect the resolution of the new better result. Meaning there is no
+;   improvement -> infinit loop.
+
